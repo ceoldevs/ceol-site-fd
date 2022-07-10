9e67596329da3c3fe3ce62591772d1abd655ca47
smc181002 <meherchaitanya18802@gmail.com>
2022-07-10 23:43:22 +0530

    added custom tailwind theme  to the code instead of arbitrary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				kuro: {
+					50: '#f1f1f1',
+          100: '#eaeaeb',
+          200: '#d6d6d7',
+          300: '#c1c1c4',
+          400: '#a3a3a6',
+          500: '#6f6f74',
+          600: '#46464d',
+          700: '#313136',
+          800: '#232327',
+          900: '#151517',
+
+				}
+			}
+		},
 	},
 	plugins: [],
 	darkMode: "class"
